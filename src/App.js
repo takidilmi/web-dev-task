6d3ad4890c2b475a795640ad440d7ec95abb19a4
taki dilmi <angeldilmi@gmail.com>
2024-02-07 19:05:02 +0100

    add responsive styling layout according the design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const imageSrc = 'https://via.placeholder.com/150';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex flex-col items-center gap-2 text-balance">
+        <div className="relative w-full">
+          <div>
+            <br></br>
+            <i className="absolute top-0 right-0 bg-red-600 py-5 px-7 clip-path-right"></i>
+          </div>
+        </div>
+        <div className="relative flex flex-col w-full">
+          <div className="self-center">
+            <div>
+              <div className="flex justify-center gap-4 flex-wrap mb-4">
+                {Array.from({ length: 2 }).map((_, i) => (
+                  <img
+                    key={i}
+                    className="mx-2"
+                    src={imageSrc}
+                    alt="Placeholder"
+                  />
+                ))}
+              </div>
+              <div className="flex justify-center gap-4 flex-wrap">
+                {Array.from({ length: 3 }).map((_, i) => (
+                  <img
+                    key={i}
+                    className="mx-2"
+                    src={imageSrc}
+                    alt="Placeholder"
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="relative w-full flex flex-col gap-2">
+          <div className="self-center bg-red-600 p-1 rounded-[4px]">
+            <h2>Development Team</h2>
+            <i className="absolute top-[-8px] left-0 bg-red-600 py-5 px-7 clip-path-left"></i>
+          </div>
+          <div className="self-center">
+            <div>
+              <div className="flex justify-center gap-4 flex-wrap mb-4">
+                {Array.from({ length: 4 }).map((_, i) => (
+                  <img
+                    key={i}
+                    className="mx-2"
+                    src={imageSrc}
+                    alt="Placeholder"
+                  />
+                ))}
+              </div>
+              <div className="flex justify-center gap-4 flex-wrap">
+                {Array.from({ length: 3 }).map((_, i) => (
+                  <img
+                    key={i}
+                    className="mx-2"
+                    src={imageSrc}
+                    alt="Placeholder"
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="relative w-full flex flex-col gap-2">
+          <div className="self-center bg-red-600 p-1 rounded-[4px]">
+            <h2>Sales and Marketing Team</h2>
+            <i className="absolute top-[-8px] right-0 bg-red-600 py-5 px-7 clip-path-right"></i>
+          </div>
+          <div className="self-center">
+            <div>
+              <div className="flex justify-center gap-4 flex-wrap mb-4">
+                {Array.from({ length: 4 }).map((_, i) => (
+                  <img
+                    key={i}
+                    className="mx-2"
+                    src={imageSrc}
+                    alt="Placeholder"
+                  />
+                ))}
+              </div>
+              <div className="flex justify-center gap-4 flex-wrap">
+                {Array.from({ length: 3 }).map((_, i) => (
+                  <img
+                    key={i}
+                    className="mx-2"
+                    src={imageSrc}
+                    alt="Placeholder"
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="relative w-full">
+          <div>
+            <br></br>
+            <i className="absolute bottom-0 left-0 bg-red-600 py-5 px-7 clip-path-left"></i>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      clipPath: {
+        right: 'polygon(100% 0, 100% 50%, 100% 100%, 0% 100%, 40% 50%, 0% 0%)',
+        left:'polygon(100% 0%, 60% 50%, 100% 100%, 0 100%, 0% 50%, 0 0);',
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    require('tailwind-clip-path'),
+  ],
+};
